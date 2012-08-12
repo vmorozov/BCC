6ad2b2c60e55e22b975b9256c887075e7f09160a
vmorozov <vmmorozov@gmail.com>
2012-08-12 19:24:40 +0000

    update BCC package

--- proc.R
+++ proc.R
@@ -1,5 +1,7 @@
 library(BCC)
-synapseLogin('','')
-d2=loadMetabricTrainingData()
-d1=loadMetabricTrainingData_federation()
+synapseLogin('vmmorozov@gmail.com','')
+d1=loadMetabricTrainingData()
+sapply(d1[1:2],function(x) {x=featureNames(x);print(list(Nf=length(x),head=head(x)))})
 sum(featureNames(d1[[1]]) %in% featureNames(d1[[2]]))
+
+#d2=loadMetabricTrainingData_federation()
